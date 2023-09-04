0884e5e30e276bdc69de23c30eb77a1622adae71
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-09-04 12:01:28 +0100

    new  main

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import FaqLatest from "@/Components/common/FaqLatest";
 import Footer from "@/Components/common/Footer";
-import Main from "@/Components/common/Main";
+// import Main from "@/Components/common/Main";
 import NavBar from "@/Components/common/NavBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Main />
+      {/* <Main /> */}
       <FaqLatest />
       <Footer />
     </>
